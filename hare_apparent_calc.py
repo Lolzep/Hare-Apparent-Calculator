0506c24848db3333a0600d123aa7675b66393046
Lolzep <kjkinzel@gmail.com>
2025-06-21 15:37:49 -0400

    Removed "amount" user input, halved code size

--- hare_apparent_calc.py
+++ hare_apparent_calc.py
@@ -11,9 +11,12 @@ l_rabbits = []
 l_copies = []
 l_etbs = []
 
-apparents = int(input("Current Number of Hare Apparents (NOT including copies): "))
-illusions = int(input("Current Number of 0/1 Illusion copies: "))
-amount = input("Are we flickering all Hare Apparents or just one? (All/1): ")
+apparents = int(input("Non-Token Hare Apparents in play being flickered: "))
+
+total = int(input("Total Hare Apparents in play (including token copies): "))
+illusions = total - apparents
+
+#amount = input("Are we flickering all Hare Apparents or just one? (All/1): ")
 preston = input("Is Preston in play for copy generation? (T/F): ")
 flicker = int(input("Amount of times to flicker: "))
 hare_repeats = int(input("How many times is Hare Apparent's triggers being repeated?: "))
@@ -31,127 +34,42 @@ real_apparents = apparents
 # Flicker X amount of times
 while i < flicker:
 # Hare Apparent leaves and enters X amount of times and repeated Y amount of times (if hare_repeats > 0)
-    if amount == "All":
-        if hare_repeats > 0:
-            for j in range(hare_repeats+1):
-                if j == min(range(hare_repeats+1)):
-                    etbs += real_apparents
-                    c_etbs = real_apparents
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if j==min(range(hare_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(j))
-                    l_source.append("HA ETB")
-                    l_rabbits.append("0")
-                    l_copies.append("0")
-                    l_etbs.append(str(c_etbs))
-                else:
-                    etbs += 0
-                    c_etbs = 0
-
-        else:
-            etbs += real_apparents
-            c_etbs = real_apparents
-
-            l_flicker.append("Flicker "+ str(i + 1))
-            if j==min(range(hare_repeats+1)):
-                l_repeats.append("Original")
-            else:
-                l_repeats.append("Repeat "+ str(j))
-            l_source.append("HA ETB")
-            l_rabbits.append("0")
-            l_copies.append("0")
-            l_etbs.append(str(c_etbs))
-    else:
-        if hare_repeats > 0:
-            for j in range(hare_repeats+1):
-                if j == min(range(hare_repeats+1)):
-                    etbs += 1
-                    c_etbs = 1
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if j==min(range(hare_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(j))
-                    l_source.append("HA ETB")
-                    l_rabbits.append("0")
-                    l_copies.append("0")
-                    l_etbs.append(str(c_etbs))
-                else:
-                    etbs += 0
-                    c_etbs = 0
-        else:
-            etbs += 1
-            c_etbs = 1
-
-            l_flicker.append("Flicker "+ str(i + 1))
-            if j==min(range(hare_repeats+1)):
-                l_repeats.append("Original")
-            else:
-                l_repeats.append("Repeat "+ str(j))
-            l_source.append("HA ETB")
-            l_rabbits.append("0")
-            l_copies.append("0")
-            l_etbs.append(str(c_etbs))
-
-# Hare Apparent's ETB triggers X amount of times and repeated Y amount of times (if hare_repeats > 0)
-    if amount == "All":
-        if hare_repeats > 0:
-            for j in range(hare_repeats+1):
-                rabbits += (real_apparents) * (apparents + illusions - 1)
-                c_rabbits = (real_apparents) * (apparents + illusions - 1)
-
-                etbs += c_rabbits
-                c_etbs = c_rabbits
+    if hare_repeats > 0:
+        for j in range(hare_repeats+1):
+            if j == min(range(hare_repeats+1)):
+                etbs += real_apparents
+                c_etbs = real_apparents
 
                 l_flicker.append("Flicker "+ str(i + 1))
                 if j==min(range(hare_repeats+1)):
                     l_repeats.append("Original")
                 else:
                     l_repeats.append("Repeat "+ str(j))
-                l_source.append("HA Trigger")
-                l_rabbits.append(str(c_rabbits))
+                l_source.append("HA ETB")
+                l_rabbits.append("0")
                 l_copies.append("0")
                 l_etbs.append(str(c_etbs))
+            else:
+                etbs += 0
+                c_etbs = 0
 
-            if preston == "T" and preston_repeats > 0:
-                for k in range(preston_repeats+1):
-                    copies += real_apparents
-                    c_copies = real_apparents
+    else:
+        etbs += real_apparents
+        c_etbs = real_apparents
 
-                    etbs += c_copies
-                    c_etbs = c_copies
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if k==min(range(preston_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(k))
-                    l_source.append("P Trigger")
-                    l_rabbits.append("0")
-                    l_copies.append(str(c_copies))
-                    l_etbs.append(str(c_etbs))
-            elif preston == "T":
-                copies += real_apparents
-                c_copies = real_apparents
-
-                etbs += c_copies
-                c_etbs = c_copies
-
-                l_flicker.append("Flicker "+ str(i + 1))
-                if k==min(range(preston_repeats+1)):
-                    l_repeats.append("Original")
-                else:
-                    l_repeats.append("Repeat "+ str(k))
-                l_source.append("P Trigger")
-                l_rabbits.append("0")
-                l_copies.append(str(c_copies))
-                l_etbs.append(str(c_etbs))
-
+        l_flicker.append("Flicker "+ str(i + 1))
+        if j==min(range(hare_repeats+1)):
+            l_repeats.append("Original")
         else:
+            l_repeats.append("Repeat "+ str(j))
+        l_source.append("HA ETB")
+        l_rabbits.append("0")
+        l_copies.append("0")
+        l_etbs.append(str(c_etbs))
+
+# Hare Apparent's ETB triggers X amount of times and repeated Y amount of times (if hare_repeats > 0)
+    if hare_repeats > 0:
+        for j in range(hare_repeats+1):
             rabbits += (real_apparents) * (apparents + illusions - 1)
             c_rabbits = (real_apparents) * (apparents + illusions - 1)
 
@@ -168,24 +86,8 @@ while i < flicker:
             l_copies.append("0")
             l_etbs.append(str(c_etbs))
 
-            if preston == "T" and preston_repeats > 0:
-                for k in range(preston_repeats+1):
-                    copies += real_apparents
-                    c_copies = real_apparents
-
-                    etbs += c_copies
-                    c_etbs = c_copies
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if k==min(range(preston_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(k))
-                    l_source.append("P Trigger")
-                    l_rabbits.append("0")
-                    l_copies.append(str(c_copies))
-                    l_etbs.append(str(c_etbs))
-            elif preston == "T":
+        if preston == "T" and preston_repeats > 0:
+            for k in range(preston_repeats+1):
                 copies += real_apparents
                 c_copies = real_apparents
 
@@ -201,47 +103,44 @@ while i < flicker:
                 l_rabbits.append("0")
                 l_copies.append(str(c_copies))
                 l_etbs.append(str(c_etbs))
+        elif preston == "T":
+            copies += real_apparents
+            c_copies = real_apparents
+
+            etbs += c_copies
+            c_etbs = c_copies
+
+            l_flicker.append("Flicker "+ str(i + 1))
+            if k==min(range(preston_repeats+1)):
+                l_repeats.append("Original")
+            else:
+                l_repeats.append("Repeat "+ str(k))
+            l_source.append("P Trigger")
+            l_rabbits.append("0")
+            l_copies.append(str(c_copies))
+            l_etbs.append(str(c_etbs))
 
     else:
-        if hare_repeats > 0:
-            for j in range(hare_repeats+1):
-                rabbits += 1 * (apparents + illusions - 1)
-                c_rabbits = 1 * (apparents + illusions - 1)
+        rabbits += (real_apparents) * (apparents + illusions - 1)
+        c_rabbits = (real_apparents) * (apparents + illusions - 1)
 
-                etbs += c_rabbits
-                c_etbs = c_rabbits
+        etbs += c_rabbits
+        c_etbs = c_rabbits
 
-                l_flicker.append("Flicker "+ str(i + 1))
-                if j==min(range(hare_repeats+1)):
-                    l_repeats.append("Original")
-                else:
-                    l_repeats.append("Repeat "+ str(j))
-                l_source.append("HA Trigger")
-                l_rabbits.append(str(c_rabbits))
-                l_copies.append("0")
-                l_etbs.append(str(c_etbs))
+        l_flicker.append("Flicker "+ str(i + 1))
+        if j==min(range(hare_repeats+1)):
+            l_repeats.append("Original")
+        else:
+            l_repeats.append("Repeat "+ str(j))
+        l_source.append("HA Trigger")
+        l_rabbits.append(str(c_rabbits))
+        l_copies.append("0")
+        l_etbs.append(str(c_etbs))
 
-            if preston == "T" and preston_repeats > 0:
-                for k in range(preston_repeats+1):
-                    copies += 1
-                    c_copies = 1
-
-                    etbs += c_copies
-                    c_etbs = c_copies
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if k==min(range(preston_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(k))
-                    l_source.append("P Trigger")
-                    l_rabbits.append("0")
-                    l_copies.append(str(c_copies))
-                    l_etbs.append(str(c_etbs))
-                    
-            elif preston == "T":
-                copies += 1
-                c_copies = 1
+        if preston == "T" and preston_repeats > 0:
+            for k in range(preston_repeats+1):
+                copies += real_apparents
+                c_copies = real_apparents
 
                 etbs += c_copies
                 c_etbs = c_copies
@@ -254,10 +153,48 @@ while i < flicker:
                 l_source.append("P Trigger")
                 l_rabbits.append("0")
                 l_copies.append(str(c_copies))
-                l_etbs.append(str(c_etbs))               
+                l_etbs.append(str(c_etbs))
+        elif preston == "T":
+            copies += real_apparents
+            c_copies = real_apparents
+
+            etbs += c_copies
+            c_etbs = c_copies
+
+            l_flicker.append("Flicker "+ str(i + 1))
+            if k==min(range(preston_repeats+1)):
+                l_repeats.append("Original")
+            else:
+                l_repeats.append("Repeat "+ str(k))
+            l_source.append("P Trigger")
+            l_rabbits.append("0")
+            l_copies.append(str(c_copies))
+            l_etbs.append(str(c_etbs))
+
+# Preston's ETB triggers X amount of times and repeated Y amount of times (if preston_repeats > 0)
+    if preston == "T":
+        apparents += c_copies * (hare_repeats + 1)
+        if hare_repeats > 0:
+            for j in range(hare_repeats+1):
+                rabbits += (c_copies) * (apparents + illusions - 1)
+                c_rabbits = (c_copies) * (apparents + illusions - 1)
+
+                etbs += c_rabbits
+                c_etbs = c_rabbits
+
+                l_flicker.append("Flicker "+ str(i + 1))
+                if j==min(range(hare_repeats+1)):
+                    l_repeats.append("Original")
+                else:
+                    l_repeats.append("Repeat "+ str(j))
+                l_source.append("Copy Triggers")
+                l_rabbits.append(str(c_rabbits))
+                l_copies.append("0")
+                l_etbs.append(str(c_etbs))
+
         else:
-            rabbits += 1 * (apparents + illusions - 1)
-            c_rabbits = 1 * (apparents + illusions - 1)
+            rabbits += (c_copies) * (apparents + illusions - 1)
+            c_rabbits = (c_copies) * (apparents + illusions - 1)
 
             etbs += c_rabbits
             c_etbs = c_rabbits
@@ -267,121 +204,10 @@ while i < flicker:
                 l_repeats.append("Original")
             else:
                 l_repeats.append("Repeat "+ str(j))
-            l_source.append("HA Trigger")
+            l_source.append("Copy Triggers")
             l_rabbits.append(str(c_rabbits))
             l_copies.append("0")
-            l_etbs.append(str(c_etbs))
-
-            if preston == "T" and preston_repeats > 0:
-                for k in range(preston_repeats+1):
-                    copies += 1
-                    c_copies = 1
-
-                    etbs += c_copies
-                    c_etbs = c_copies
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if k==min(range(preston_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(k))
-                    l_source.append("P Trigger")
-                    l_rabbits.append("0")
-                    l_copies.append(str(c_copies))
-                    l_etbs.append(str(c_etbs))
-            elif preston == "T":
-                copies += 1
-                c_copies = 1
-
-                etbs += c_copies
-                c_etbs = c_copies
-
-                l_flicker.append("Flicker "+ str(i + 1))
-                if k==min(range(preston_repeats+1)):
-                    l_repeats.append("Original")
-                else:
-                    l_repeats.append("Repeat "+ str(k))
-                l_source.append("P Trigger")
-                l_rabbits.append("0")
-                l_copies.append(str(c_copies))
-                l_etbs.append(str(c_etbs))
-
-# Preston's ETB triggers X amount of times and repeated Y amount of times (if preston_repeats > 0)
-    if preston == "T":
-        if amount == "All":
-            apparents += c_copies * (hare_repeats + 1)
-            if hare_repeats > 0:
-                for j in range(hare_repeats+1):
-                    rabbits += (c_copies) * (apparents + illusions - 1)
-                    c_rabbits = (c_copies) * (apparents + illusions - 1)
-
-                    etbs += c_rabbits
-                    c_etbs = c_rabbits
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if j==min(range(hare_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(j))
-                    l_source.append("Copy Triggers")
-                    l_rabbits.append(str(c_rabbits))
-                    l_copies.append("0")
-                    l_etbs.append(str(c_etbs))
-
-            else:
-                rabbits += (c_copies) * (apparents + illusions - 1)
-                c_rabbits = (c_copies) * (apparents + illusions - 1)
-
-                etbs += c_rabbits
-                c_etbs = c_rabbits
-
-                l_flicker.append("Flicker "+ str(i + 1))
-                if j==min(range(hare_repeats+1)):
-                    l_repeats.append("Original")
-                else:
-                    l_repeats.append("Repeat "+ str(j))
-                l_source.append("Copy Triggers")
-                l_rabbits.append(str(c_rabbits))
-                l_copies.append("0")
-                l_etbs.append(str(c_etbs))
-
-        else:
-            apparents += c_copies * (hare_repeats + 1)
-            if hare_repeats > 0:
-                for j in range(hare_repeats+1):
-                    rabbits += (c_copies) * (apparents + illusions - 1)
-                    c_rabbits = (c_copies) * (apparents + illusions - 1)
-
-                    etbs += c_rabbits
-                    c_etbs = c_rabbits
-
-                    l_flicker.append("Flicker "+ str(i + 1))
-                    if j==min(range(hare_repeats+1)):
-                        l_repeats.append("Original")
-                    else:
-                        l_repeats.append("Repeat "+ str(j))
-                    l_source.append("Copy Triggers")
-                    l_rabbits.append(str(c_rabbits))
-                    l_copies.append("0")
-                    l_etbs.append(str(c_etbs))
-
-            else:
-                rabbits += (c_copies) * (apparents + illusions - 1)
-                c_rabbits = (c_copies) * (apparents + illusions - 1)
-
-                etbs += c_rabbits
-                c_etbs = c_rabbits
-
-                l_flicker.append("Flicker "+ str(i + 1))
-                if j==min(range(hare_repeats+1)):
-                    l_repeats.append("Original")
-                else:
-                    l_repeats.append("Repeat "+ str(j))
-                l_source.append("Copy Triggers")
-                l_rabbits.append(str(c_rabbits))
-                l_copies.append("0")
-                l_etbs.append(str(c_etbs))
-                
+            l_etbs.append(str(c_etbs))  
     i += 1
 
 # append all lists into a list of lists where each list is a column in the final table
